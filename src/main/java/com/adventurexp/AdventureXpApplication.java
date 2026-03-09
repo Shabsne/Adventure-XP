645796135c03a65b846b24c445cc9d7c64c963bb
Uller <fullersted@gmail.com>
2026-03-09 09:30:43 +0100

    Added main class

--- src/main/java/com/adventurexp/AdventureXpApplication.java
+++ src/main/java/com/adventurexp/AdventureXpApplication.java
@@ -1,4 +1,9 @@
 package com.adventurexp;
 
+import org.springframework.boot.SpringApplication;
+
 public class AdventureXpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureXpApplication.class,args);
+    }
 }
